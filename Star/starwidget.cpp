7ea2881411fa62db93b2f01f5810641a53753aac
Ade <13588028591@163.com>
2016-07-06 23:08:35 +0800

    Star Project without annotation

--- Star/starwidget.cpp
+++ Star/starwidget.cpp
@@ -6,9 +6,86 @@ StarWidget::StarWidget(QWidget *parent) :
     ui(new Ui::StarWidget)
 {
     ui->setupUi(this);
+    for(int i = 0; i < 100; i++)
+    {
+        x[i] = qrand() % width();
+        y[i] = -(qrand() % height());
+    }
+    dayTimeFlag = true;
+    startTimer(80);
+    moonFlag = false;
+    resize(1024, 768);
 }
 
 StarWidget::~StarWidget()
 {
     delete ui;
+}
+
+void StarWidget::paintEvent(QPaintEvent *e)
+{
+    Q_UNUSED(e)
+
+    QPainter p(this);
+
+    if(moonFlag && !dayTimeFlag)
+    {
+        p.setBrush(Qt::white);
+        p.setPen(Qt::black);
+        p.drawEllipse(100, 100, 80, 80);
+        p.setBrush(Qt::black);
+        p.setPen(Qt::black);
+        p.drawEllipse(90, 80, 80, 80);
+    }
+
+    if(dayTimeFlag)
+    {
+        setStyleSheet("background-color: rgb(255, 255, 255);");
+        p.setPen(Qt::black);
+    }
+    else
+    {
+        setStyleSheet("background-color: rgb(0, 0, 0);");
+        p.setPen(Qt::white);
+    }
+
+
+
+    for(int i = 0; i < 100; i++)
+    {
+        p.drawText(x[i], y[i], "*");
+    }
+
+}
+
+void StarWidget::timerEvent(QTimerEvent *e)
+{
+    Q_UNUSED(e)
+
+    for(int i = 0; i < 100; i++)
+    {
+        y[i] += 10;
+        if(y[i] > height() - 10)
+        {
+            y[i] = -(qrand() % height());
+            x[i] = qrand() % width();
+        }
+    }
+
+    update();
+}
+
+void StarWidget::on_radioButtonDaytime_clicked()
+{
+    dayTimeFlag = true;
+}
+
+void StarWidget::on_radioButtonNight_clicked()
+{
+    dayTimeFlag = false;
+}
+
+void StarWidget::on_checkBox_clicked()
+{
+    moonFlag = !moonFlag;
 }
--- Star/starwidget.h
+++ Star/starwidget.h
@@ -2,6 +2,8 @@
 #define STARWIDGET_H
 
 #include <QWidget>
+#include <QPainter>
+#include <QTimer>
 
 namespace Ui {
 class StarWidget;
@@ -14,9 +16,22 @@ class StarWidget : public QWidget
 public:
     explicit StarWidget(QWidget *parent = 0);
     ~StarWidget();
+    void paintEvent(QPaintEvent *e);
+    void timerEvent(QTimerEvent *e);
+
+private slots:
+    void on_radioButtonDaytime_clicked();
+
+    void on_radioButtonNight_clicked();
+
+    void on_checkBox_clicked();
 
 private:
     Ui::StarWidget *ui;
+    bool dayTimeFlag;
+    bool moonFlag;
+    int x[100];
+    int y[100];
 };
 
 #endif // STARWIDGET_H
